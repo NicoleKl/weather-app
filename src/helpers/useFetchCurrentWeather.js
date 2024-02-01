6e43d31ff1448cffc78511200ea76d573a7617f2
nika <nika@Veronikas-MacBook-Pro.local>
2024-01-31 20:06:08 -0800

    http to https

--- src/helpers/useFetchCurrentWeather.js
+++ src/helpers/useFetchCurrentWeather.js
@@ -4,7 +4,7 @@ import { useState, useEffect, useCallback } from "react";
 export function useFetchCurrentWeather(coordinates, units) {
     const lat = (Math.round(coordinates[1] * 100) / 100).toFixed(2); //london
     const lon = (Math.round(coordinates[0] * 100) / 100).toFixed(2); //london http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=
-    const apiUrl = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=`;
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=`;
     const ApiKey = 'fa43aa6d7f9b139f3047f2710672eea3';
 
     const [loadingCurrentWeather, setLoading] = useState(true);
